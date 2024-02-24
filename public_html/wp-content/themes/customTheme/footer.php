c1ff4009caba5e8acc0c7614190d21ff39f6c25e
trixiamarie <trixialorenzana@gmail.com>
2024-02-24 17:42:53 +0100

    aaaaah

---
+++ public_html/wp-content/themes/customTheme/footer.php
@@ -0,0 +1,58 @@
+<footer class="absolute-bottom">
+    <div class="row my-3 align-items-start">
+        <div class="col-md-3 text-center"><img src="http://nuotaconnoi.netsons.org/wp-content/uploads/2024/02/d3d2c3a4-3543-4889-970d-bb699a1ef299.jpg" width="150px"></div>
+        <div class="col-md-6 text-center">
+            <ul class="list-unstyled d-flex justify-content-evenly flex-wrap">
+                <li><a href="#" class="text-decoration-none">Meetings</a></li>
+                <li><a href="#" class="text-decoration-none">Groups & Sports</a></li>
+                <li><a href="#" class="text-decoration-none">Travel Agents</a></li>
+                <li><a href="#" class="text-decoration-none">Press</a></li>
+                <li><a href="#" class="text-decoration-none">Department of Aviation</a></li>
+                <li><a href="#" class="text-decoration-none">Contact</a></li>
+                <li><a href="#" class="text-decoration-none">Privacy Policy</a></li>
+                <li><a href="#" class="text-decoration-none">Accessibility</a></li>
+                <li><a href="#" class="text-decoration-none">Sitemap</a></li>
+                <li><a href="#" class="text-decoration-none">Consent Preferences</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 text-center d-flex socialicon">
+    <a href="#"><i class="bi bi-instagram"></i></a>
+    <a href="#"><i class="bi bi-facebook"></i></a>
+    <a href="#"><i class="bi bi-youtube"></i></a>
+    <a href="#"><i class="bi bi-twitter-x"></i></a>
+    <a href="#"><i class="bi bi-pinterest"></i></a>
+</div>
+
+    </div>
+    <div class="row my-5">
+        <div class="col-md-3 text-center intile"></div> 
+        <div class="col-md-6 text-center">
+            <h3 class="text-center">VISIT OUR PARTNER WEBSITES</h3>
+            <div class="d-flex finalicon">
+                <div><img class="logo" src="http://nuotaconnoi.netsons.org/wp-content/uploads/2024/02/logonassau2-e1708697888405.png" alt=""></div>
+                <div><img class="logo" src="http://nuotaconnoi.netsons.org/wp-content/uploads/2024/02/logo-the-outislands.svg" alt=""></div>
+                <div><img class="logo" src="http://nuotaconnoi.netsons.org/wp-content/uploads/2024/02/logo-grand-bahama-island.svg" alt=""></div>
+            </div>
+        </div>
+        <div class="col-md-3 text-center"><p>Download our App</p>
+           <a href="#"> <img class ="img-app"src="http://nuotaconnoi.netsons.org/wp-content/uploads/2024/02/app.svg" alt=""></a>
+        </div>
+    </div>
+
+    <div class="row ultimatetext">
+   <div class="col-md-3 text-center">Bahamas Tourism Center (800) 224-2627</div>
+   <div class="col-md-6 text-center">© 2024 Bahamas Ministry of Tourism All Rights Reserved</div>
+   <div class="col-md-3 text-center"></div>
+
+
+
+    </div>
+    <div class="card text-center absolute-bottom my-3">
+        <div class="card-footer text-body-secondary">
+            <p class="text-center">Copyright &copy; <?php echo date("Y"); ?> ~ Trixia, Anna, Gregorio, Marco, Manuel, Monica, Lavinia
+        </div>
+</footer>
+</body>
+</html>
+    <?php wp_footer() ?> <!--  Hook che gestisce caricamente nell'header della pagina -->
+
---
+++ public_html/wp-content/themes/customTheme/front-page.php
@@ -0,0 +1,124 @@
+<!-- PERCHE' FRONT-PAGE?  legga=> https://wordpress.stackexchange.com/questions/110981/what-is-the-difference-between-front-page-php-and-home-php -->
+<?php get_header(); ?>
+
+<div class="container container-fluid homePage">
+  <h1 id="nomeSito">
+  <?php bloginfo('name'); ?>
+  </h1>
+  <h2 id="mottoSito"><?php bloginfo('description'); ?></h2>
+  <video class="videoHeader" playsinline="" loop="" autoplay="" muted="" preload="none"
+    poster="https://www.bahamas.com/assets/images/home-video-poster.jpg">
+    <source src="https://tempo.cdn.tambourine.com/windsong/media/bmot-website-homepage-header-desktop-658dd8a691a47.mp4"
+      type="video/mp4; codecs=&quot;avc1.4D401E, mp4a.40.2&quot;">
+  </video>
+
+
+
+  <div class="my-5">
+
+    <div class="card mb-3 cardWithP border-0 bg-transparent">
+      <div class="row g-0">
+        <div class="col-md-6">
+          <div class="card-body">
+            <p class="card-text fs-4 homepageP">Vieni a gustare l'eccitante esperienza delle Bahamas: dove il sole cocente ti
+              abbraccia come un polipo, le acque cristalline ti accolgono con la gentilezza di uno
+              squalo affamato e i resort lussuosi ti faranno sentire come un granchio fuori dall'acqua. Un'opportunità
+              per rilassarsi, abbronzarsi e rischiare di diventare l'aperitivo di un'iguana assetata.</p>
+          </div>
+        </div>
+        <div class="col-md-6" style="height: 250px;">
+          <img class="img-fluid rounded-start"
+            src="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/motorino.jpg" />
+        </div>
+      </div>
+    </div>
+
+    <div class="card text-white mb-3 rounded">
+      <img class="object-fit-cover" src="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/indigeni-felici.jpg""
+                class=" card-img h-100" alt="...">
+      <div class="card-img-overlay">
+        <h5 class="card-title fs-1 w-50 mt-5">VERO SPIRITO DELLE BAHAMAS</h5>
+        <p class="card-text fs-4 w-50">Beh, i locali alle Bahamas sono come dei veri e propri maestri del relax: sempre
+          con un sorriso stampato in faccia, pronti a darti il benvenuto con un "Hello, mon!" che suona tanto genuino
+          quanto una pubblicità di dentifricio. Sono maestri nell'arte di prendersela comoda, tanto che sembrano vivere
+          in un eterno "island time", dove le lancette dell'orologio sembrano avere una vita propria. E sì, potresti
+          persino convincerti che la parola "stress" non esista nel loro vocabolario, ma non ti preoccupare, sono abili
+          nel trasmetterti una calma rilassante mentre controlli il tuo saldo bancario prima di ordinare un altro
+          cocktail a prezzi esorbitanti. </p>
+      </div>
+    </div>
+
+    <div class="card mb-3 cardWithP w-100 bg-transparent">
+      <div class="row g-0">
+        <div class="col-lg-9">
+          <img class="object-fit-cover rounded-start"
+            src="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/casette-sostenibili.jpg" />
+        </div>
+        <div class="col-lg-3">
+          <div class="card-body">
+            <h5 class="card-title fs-2">Lussuosi alloggi in paglia riciclata</h5>
+            <p class="card-text w-100">Ogni dettaglio è curato con precisione per offrire un'esperienza di soggiorno
+              indimenticabile: dalle suite private con terrazze panoramiche alle ville esclusive eco-sostenibili. Con un
+              servizio impeccabile e una cornice naturale incomparabile, gli alloggi alle Bahamas trasformano ogni
+              momento in un'opportunità per ritrovare la pace.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--     <div class="card text-white mb-3 border-0">
+      <div class="row">
+        <img class="object-fit-cover col-6"
+          src="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/lavitaloro.jpg" class=" card-img h-100"
+          alt="...">
+        <img class="object-fit-cover col-6 position-relative"
+          src="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/lavitaloroagua.jpg" class=" card-img h-100"
+          alt="...">
+          <div class="card-img-overlay w-50 bg-info position-absolute end-0">
+        <p class="card-text fs-4 w-100 lavitaloroText bg-danger">Esplorare le Bahamas è un'esperienza unica. Se il tuo portafoglio non risplende
+          di opulenza, potresti voler ripensare alla tua destinazione di viaggio. Queste isole incantate, immerse nelle
+          acque turchesi dell'oceano, sono un rifugio per coloro che non conoscono il significato della parola "budget".
+          Sì, la bellezza è palpabile, ma anche il prezzo. Tuttavia, se hai il saldo del conto in banca giusto,
+          preparati a immergerti in un mondo di lusso e tranquillità senza pari. Se invece hai solo monete perdute nel
+          divano, forse è meglio considerare alternative più economiche.</p>
+      </div>
+      </div>
+     
+    </div> -->
+
+
+    <div class="card mb-3 cardWithP">
+      <div class="row g-0">
+        <div class="col-md-6">
+          <div class="card-body">
+            <h5 class="card-title">Nuotare con i maiali alle Bahamas, un'esperienza così unica che potresti
+              pensare che il destination managaer abbia fatto troppo affidamento sull'immaginazione durante la
+              progettazione
+              delle attività turistiche </h5>
+            <p class="card-text">Immagina di fare snorkeling con Babe! Sì, hai capito
+              bene, stiamo parlando di nuotare con i maiali! È come se la fattoria si trasferisse
+              direttamente nell'oceano, con i maiali che si sono diplomati alla scuola di nuoto di Aquaman. Mentre ti
+              tuffi nell'acqua turchese, ti accorgi che questi suini acquatici sono più alla moda di te, sfoggiando i
+              loro migliori costumi da bagno fatti di pelle grinzosa e con un atteggiamento da divi di Hollywood. E
+              mentre ti osservano con sguardo curioso, non puoi fare a meno di pensare che al contrario di te i maiali
+              conoscono
+              davvero il segreto della felicità: immergersi nelle acque cristalline e prendersela comoda al sole.</p>
+          </div>
+        </div>
+        <div class="col-md-6 h-100">
+          <img class="img-fluid rounded-end"
+            src="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/maialino.jpg">
+        </div>
+      </div>
+    </div>
+
+    <div class="card cardMap">
+      <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/agua.jpg">
+      <?php include get_parent_theme_file_path('template-parts/bahamasMap.php') ?>
+    </div>
+
+  </div>
+
+</div>
+
+<?php get_footer(); // hook di WP per elaborare ed includere il file footer.php           ?>
